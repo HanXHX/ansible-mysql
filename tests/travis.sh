121ce274560b9883af4959a60e073b7a6ede5759
Emilien Mantel <emilien.mantel@debianiste.org>
2016-09-25 14:43:32 +0200

    Change travis.sh (help idempotence debug)

--- tests/travis.sh
+++ tests/travis.sh
@@ -15,7 +15,7 @@ ansible-playbook -i $INVENTORY_FILE -c local --syntax-check -vv $PLAYBOOK
 ansible-playbook -i $INVENTORY_FILE -c local -e "{ mysql_vendor: $VENDOR, mysql_origin: $ORIGIN }" --sudo -vv $PLAYBOOK
 
 # Check indempotence
-ansible-playbook -i $INVENTORY_FILE -c local -e "{ mysql_vendor: $VENDOR, mysql_origin: $ORIGIN }" --sudo -vv $PLAYBOOK \
-| grep -q 'changed=0.*failed=0' \
+ansible-playbook -i $INVENTORY_FILE -c local -e "{ mysql_vendor: $VENDOR, mysql_origin: $ORIGIN }" --sudo -vv $PLAYBOOK > idempot.txt
+grep -q 'changed=0.*failed=0' idempot.txt \
 && (echo 'Idempotence test: pass' && exit 0) \
-|| (echo 'Idempotence test: fail' && exit 1)
+|| (echo 'Idempotence test: FAIL' && cat idempot.txt && exit 1)
